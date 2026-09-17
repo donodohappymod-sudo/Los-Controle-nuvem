1599e9627ddb77ae3039e015320a0c0c511229e9
donodohappymod-sudo <donodohappymod@gmail.com>
2026-09-17 19:08:12 -0300

    Excluir app__layout.tsx

--- app__layout.tsx
+++
@@ -1,4 +0,0 @@
-import './globals.css';
-import type { Metadata } from 'next';
-export const metadata:Metadata={title:'LOS COLLECTOR',description:'Collect • Organize • Monitor'};
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="pt-BR"><body>{children}</body></html>}
